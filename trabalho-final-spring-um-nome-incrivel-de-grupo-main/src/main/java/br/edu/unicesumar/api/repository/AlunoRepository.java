fb5fe4df3785f815ad0f40beeb3dab8e04ef44ba
Leonardo Leitao <102124645+LeoLeitao13@users.noreply.github.com>
2025-06-19 20:54:42 -0300

    Delete trabalho-final-spring-um-nome-incrivel-de-grupo-main directory

--- trabalho-final-spring-um-nome-incrivel-de-grupo-main/src/main/java/br/edu/unicesumar/api/repository/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.unicesumar.api.repository;
-
-import br.edu.unicesumar.api.entity.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-}
